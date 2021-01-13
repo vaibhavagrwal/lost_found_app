3b94b7de175af90e5b9b7ef1dfd5491416351d50
debugger13524 <vaibhavagarwal306@gmail.com>
2021-01-14 00:48:06 +0530

    Login Complete

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       home: screen,
     );
   }
